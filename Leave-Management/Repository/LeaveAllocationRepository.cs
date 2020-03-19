af03e7a6a19d9edcf878f0188b07956a2944bc47
Kristóf Balogh <aerandir117@gmail.com>
2020-03-19 23:30:33 +0100

    ?

---
+++ Leave-Management/Repository/LeaveAllocationRepository.cs
@@ -0,0 +1,65 @@
+﻿using Leave_Management.Contracts;
+using Leave_Management.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Leave_Management.Repository
+{
+    public class LeaveAllocationRepository : ILeaveAllocationRepository
+    {
+
+        private readonly ApplicationDbContext _db;
+
+        public LeaveAllocationRepository(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public bool Create(LeaveAllocation entity)
+        {
+            _db.LeaveAllocations.Add(entity);
+            //save
+            return Save();
+            //throw new NotImplementedException();
+        }
+
+        public bool Delete(LeaveAllocation entity)
+        {
+            _db.LeaveAllocations.Remove(entity);
+            //save
+            return Save();
+            //throw new NotImplementedException();
+        }
+
+        public ICollection<LeaveAllocation> FindAll()
+        {
+            var leaveAllocations = _db.LeaveAllocations.ToList();
+            return leaveAllocations;
+           // throw new NotImplementedException();
+        }
+
+        public LeaveAllocation FindById(int id)
+        {
+            var leaveAllocation = _db.LeaveAllocations.Find(id);
+            return leaveAllocation;
+            //throw new NotImplementedException();
+        }
+
+        public bool Save()
+        {
+            var changes = _db.SaveChanges();
+            return changes > 0;
+            //throw new NotImplementedException();
+        }
+
+        public bool Update(LeaveAllocation entity)
+        {
+            _db.LeaveAllocations.Update(entity);
+            //save
+            return Save();
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Leave-Management/Repository/LeaveTypeRepository.cs
@@ -0,0 +1,74 @@
+﻿using Leave_Management.Contracts;
+using Leave_Management.Data;
+using Microsoft.AspNetCore.Identity.UI.V3.Pages.Internal.Account.Manage;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Threading.Tasks;
+
+namespace Leave_Management.Repository
+{
+    public class LeaveTypeRepository : ILeaveTypeRepository
+    {
+        private readonly ApplicationDbContext _db;
+
+        public LeaveTypeRepository(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+
+        public bool Create(LeaveType entity)
+        {
+            _db.LeaveTypes.Add(entity);
+            //save
+            return Save();
+            //throw new NotImplementedException();
+        }
+
+        public bool Delete(LeaveType entity)
+        {
+            _db.LeaveTypes.Remove(entity);
+            //save
+            return Save();
+            //throw new NotImplementedException();
+        }
+
+        public ICollection<LeaveType> FindAll()
+        {
+            var leaveTypes = _db.LeaveTypes.ToList();
+            return leaveTypes;
+            //vagy
+            //return _db.LeaveTypes.ToList();
+           // throw new NotImplementedException();
+        }
+
+        public LeaveType FindById(int id)
+        {
+            var leaveType = _db.LeaveTypes.Find(id);
+            return leaveType;
+            //throw new NotImplementedException();
+        }
+
+        public ICollection<LeaveType> GetEmployeesByLeaveType(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Save()
+        {
+            var changes = _db.SaveChanges();
+            return changes > 0;
+            //throw new NotImplementedException();
+        }
+
+        public bool Update(LeaveType entity)
+        {
+            _db.LeaveTypes.Update(entity);
+            //save
+            return Save();
+           // throw new NotImplementedException();
+        }
+    }
+}
